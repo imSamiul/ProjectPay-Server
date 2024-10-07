c26d49f7862b4d7b63d1d8d21831ba5fb9f1409b
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-10-07 17:33:30 +0600

    added pageParam for skip and limit for fix amount of project shows in getManagerProjects()

--- src/controller/managerController.ts
+++ src/controller/managerController.ts
@@ -4,8 +4,13 @@ import Project from '../model/projectModel';
 // GET:
 // get all projects for manager
 export async function getManagerProjects(req: Request, res: Response) {
+  const { pageParam = 1, limit = 10 } = req.query;
+  const pageNumber = Number(pageParam);
+  const limitNumber = Number(limit);
   try {
-    const projects = await Project.find({ projectManager: req.user?._id });
+    const projects = await Project.find({ projectManager: req.user?._id })
+      .skip((pageNumber - 1) * limitNumber)
+      .limit(limitNumber);
     res.status(200).send({ projects });
   } catch (error) {
     let errorMessage = 'Failed to do something exceptional';
