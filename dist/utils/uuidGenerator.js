f736185a94b9b44387841835a2f7c708364ff3b2
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-10-27 18:31:59 +0600

    Remove unused type files and configuration files

--- dist/utils/uuidGenerator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUUID = generateUUID;
-const uuid_1 = require("uuid");
-function generateUUID() {
-    const id = (0, uuid_1.v4)();
-    return id.toString().substring(0, 8);
-}
