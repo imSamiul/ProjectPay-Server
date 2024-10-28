e5d0ba8a4098cd24e21a46edb3a2d9bbc44e9ef4
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-10-28 14:23:50 +0600

    refactor userController.ts for better error message

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -3,38 +3,35 @@ import Client from '../model/clientModel';
 import ProjectManager from '../model/managerModel';
 import User from '../model/userModel';
 
-// GET:
-
-// Get all clients
+// GET: Get all clients
 export async function getClientList(req: Request, res: Response) {
   try {
     const clientsList = await Client.find();
-    res.status(200).send({ clientsList });
+    res.status(200).json({ clientsList });
   } catch (error) {
     let errorMessage = 'Failed to load the client list';
     if (error instanceof Error) {
       errorMessage = error.message;
     }
 
-    res.status(500).send({ message: errorMessage });
+    res.status(500).json({ message: errorMessage });
   }
 }
-// Get the user details
+// GET: Get the user details
 export async function getUserDetails(req: Request, res: Response) {
   try {
-    res.status(200).send({ user: req.user });
+    res.status(200).json({ user: req.user });
   } catch (error) {
     let errorMessage = 'Failed to load the client list';
     if (error instanceof Error) {
       errorMessage = error.message;
     }
 
-    res.status(500).send({ message: errorMessage });
+    res.status(500).json({ message: errorMessage });
   }
 }
 
-// POST:
-// Create a new client
+// POST: Create a new client
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, phone, password, userType } = req.body;
   console.log(req.body);
@@ -61,30 +58,30 @@ export const createUser = async (req: Request, res: Response) => {
 
         break;
       default:
-        return res.status(400).send('Invalid user type');
+        return res.status(400).json({ message: 'Invalid user type' });
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).send('Email already exists');
+      return res.status(400).json({ message: 'Email already exists' });
     }
 
     if (!newUser) {
-      return res.status(500).send('Failed to create user');
+      return res.status(500).json({ message: 'Failed to create user' });
     }
     const token = await newUser.generateAuthToken();
     if (!token) {
-      return res.status(500).send('Failed to generate token');
+      return res.status(500).json({ message: 'Failed to generate token' });
     }
     const savedUser = await newUser.save();
-    res.status(201).send({ user: savedUser, token });
+    res.status(201).json({ user: savedUser, token });
   } catch (error) {
     let errorMessage = 'Failed to do something exceptional';
     if (error instanceof Error) {
       errorMessage = error.message;
     }
 
-    res.status(500).send({ message: errorMessage });
+    res.status(500).json({ message: errorMessage });
   }
 };
 
@@ -94,17 +91,17 @@ export async function loginUser(req: Request, res: Response) {
   try {
     const loginSuccessful = await User.findByCredentials(email, password);
     if (!loginSuccessful) {
-      return res.status(400).send('Invalid email or password');
+      return res.status(400).json({ message: 'Invalid email or password' });
     }
     const token = await loginSuccessful.generateAuthToken();
-    res.status(201).send({ user: loginSuccessful, token });
+    res.status(201).json({ user: loginSuccessful, token });
   } catch (error) {
     let errorMessage = 'Failed to do something exceptional';
     if (error instanceof Error) {
       errorMessage = error.message;
     }
 
-    res.status(500).send({ message: errorMessage });
+    res.status(500).json({ message: errorMessage });
   }
 }
 // Logout User
@@ -121,14 +118,14 @@ export async function logOutUser(req: Request, res: Response) {
       return token.token !== req.token;
     });
     await req.user!.save();
-    res.status(200).send('Logged out successfully');
+    res.status(200).json('Logged out successfully');
   } catch (error) {
     let errorMessage = 'Failed to do something exceptional';
     if (error instanceof Error) {
       errorMessage = error.message;
     }
 
-    res.status(500).send({ message: errorMessage });
+    res.status(500).json({ message: errorMessage });
   }
 }
 
